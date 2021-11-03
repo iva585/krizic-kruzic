5b409db20bb391a705deb53a3457ae16457c970d
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-03 14:32:11 +0100

    react tutorial

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+function Square(props: {
+  onClick: () => void;
+  value:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}): JSX.Element {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
