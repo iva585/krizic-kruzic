5ed213fe3c487210eb28cc940946631e6291e277
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-03 14:43:04 +0100

    fix/refactor square function

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-function Square(props: { onClick: () => void; value: string }): JSX.Element {
+type SquareProps = { onClick: () => void; value: string };
+
+function Square(props: SquareProps): JSX.Element {
   return (
     <button className="square" onClick={() => props.onClick()}>
       {props.value}
